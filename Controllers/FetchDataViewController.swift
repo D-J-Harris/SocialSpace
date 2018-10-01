298472fc9b84e610eab5e5500d59eca6f9b083e8
Daniel Harris <09harrisda@gmail.com>
2018-10-01 22:11:16 +0100

    set up hierarchy and simple load screen

--- Controllers/FetchDataViewController.swift
+++ Controllers/FetchDataViewController.swift
@@ -1,20 +1,20 @@
 //
-//  ViewController.swift
+//  FetchDataViewController.swift
 //  SocialSpace
 //
 //  Created by Daniel Harris on 01/10/2018.
 //  Copyright © 2018 Daniel Harris. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class FetchDataViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
-
---
+++ Controllers/MembersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MembersViewController.swift
+//  SocialSpace
+//
+//  Created by Daniel Harris on 01/10/2018.
+//  Copyright © 2018 Daniel Harris. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MembersViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
